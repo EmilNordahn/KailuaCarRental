bd702242a43e9859280b01c0fa27db0eb2cb330d
caerKEA <58211711+caerKEA@users.noreply.github.com>
2020-04-02 14:47:24 +0200

    menu

--- src/AccessingDB.java
+++ src/AccessingDB.java
@@ -10,7 +10,8 @@ public class AccessingDB {
             con = null;
             Statement s = null;
             Class.forName(JDBC_DRIVER);
-            con = DriverManager.getConnection(DATABASE_URL, "root","3201516950e");
+            con = DriverManager.getConnection(DATABASE_URL, "root","Abcd12345");
+            //passwords. emil "3201516950e" - daniel "Abcd12345" - mikkel ""
             s = con.createStatement();
 
             ResultSet rs = s.executeQuery("SELECT carType, carBrand, carModel, licenseplate," +
---
+++ src/Menu.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Menu {
+    public static void MenuSelection() {
+        System.out.println("What would you like to work with?\n1. Cars\n2. Customers\n3. Rental contracts\n4. Exit program");
+        switch(getInt()) {
+            case 1:
+                System.out.println("Cars go vroom");
+                break;
+        }
+    }
+
+    public static int getInt() {
+        Scanner console = new Scanner(System.in);
+        while (!console.hasNextInt()) {
+            System.out.println("Input wasn't an integer.");
+            console.next();
+        }
+        return console.nextInt();
+    }
+}
