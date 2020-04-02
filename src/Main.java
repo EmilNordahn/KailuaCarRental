3e1433d34397fed4617659ec974bb5038ed864e0
EmilNordahn <60731699+EmilNordahn@users.noreply.github.com>
2020-04-02 15:09:24 +0200

    Fjernet kommentarer

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,5 @@ public class Main {
     public static void main(String[] args) {
         AccessingDB.listCars();
         AccessingDB.addCar();
-
-        //testpush
-        //hello from daniel
     }
 }
