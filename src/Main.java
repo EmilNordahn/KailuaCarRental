5f0c5228be38145fbf005bd4374a6bd6af3a3689
EmilNordahn <60731699+EmilNordahn@users.noreply.github.com>
2020-04-16 09:37:00 +0200

    Slettet ubrugte imports

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,3 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Kailua Car Rental!");
